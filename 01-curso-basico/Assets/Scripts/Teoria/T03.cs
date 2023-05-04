6dcc7eb3727e4a0ca758b1b343f3c1153fdadde7
HectorMartinezCred <hector.martinez.cred@outlook.com>
2023-05-04 16:28:05 -0500

    Tercera clase

---
+++ 01-curso-basico/Assets/Scripts/Teoria/T03.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T03 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Utilizar esto para la inicialización
+        Debug.Log("Primer mensaje");
+
+        /* Esto es un ejemplo de comentario multilinea
+         * hasta que se encuentre con un */
+        Debug.Log("Segundo mensaje");
+        Debug.Log("Tercer mensaje");
+        Debug.Log("Cuarto mensaje");
+        Debug.Log("Quinto mensaje");
+    }
+}
