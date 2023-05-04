ad8e9bdd085f36dc1fc79bec949b5bddf2bdc556
HectorMartinezCred <hector.martinez.cred@outlook.com>
2023-05-04 16:04:35 -0500

    Primer Clase

--- 01-curso-basico/Assets/Scripts/Ejercicios/E01.cs
+++ 01-curso-basico/Assets/Scripts/Ejercicios/E01.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class T01 : MonoBehaviour
+public class E01 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("¡Hola mundo!");
+        Debug.Log("¡Hola Hector!, ¿Como te encuentras?");
     }
-
 }
