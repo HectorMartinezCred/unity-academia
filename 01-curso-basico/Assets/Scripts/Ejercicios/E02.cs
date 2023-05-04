8ff0450ded0dd9f48c515792516cfacfb68f533a
HectorMartinezCred <hector.martinez.cred@outlook.com>
2023-05-04 16:15:21 -0500

    Segunda Clase

---
+++ 01-curso-basico/Assets/Scripts/Ejercicios/E02.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E02 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(1);
+        Debug.Log(2);
+        Debug.Log(3);
+        Debug.Log(4);
+        Debug.Log(5);
+    }
+}
