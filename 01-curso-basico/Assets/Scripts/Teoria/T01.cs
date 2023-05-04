abda6e3f10105710ce51584a413ef4dada5c5404
HectorMartinezCred <hector.martinez.cred@outlook.com>
2023-05-04 16:01:16 -0500

    Se agregan ejercicios

---
+++ 01-curso-basico/Assets/Scripts/Teoria/T01.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T01 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("¡Hola mundo!");
+        Debug.Log("Weeeee!");
+    }
+
+}
