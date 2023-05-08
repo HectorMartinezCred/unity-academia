51c7899f1ea0820a360a4633f344567443692b49
HectorMartinezCred <hector.martinez.cred@outlook.com>
2023-05-08 17:37:31 -0500

    02-Añadiendo movimiento al jugador

---
+++ 02-roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Declare variables
+    Rigidbody rb;
+    public float velocity;
+
+    private void Awake()
+    {
+        //Get Rigidbody
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Get movements
+        float movHorizontal = Input.GetAxis("Horizontal");
+        float movVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(movHorizontal, 0f, movVertical);
+
+        // Edit forces
+        rb.AddForce(movement * velocity);
+    }
+}
